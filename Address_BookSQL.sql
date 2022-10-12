874aeef3588f973248bf0e391f5f60250b16c9ed
29payal <111033408+29payal@users.noreply.github.com>
2022-10-12 12:32:17 +0530

    Update Address_BookSQL.sql

--- Address_BookSQL.sql
+++ Address_BookSQL.sql
@@ -2,3 +2,17 @@
 create database Address_Book
 
 use Address_Book
+
+------UC2 create table-----
+create table ADDRESS_BOOK
+(
+Id int identity(1,1) primary key,
+FirstName varchar(20),
+LastName varchar (20),
+Address varchar(50),
+City varchar(20),
+State varchar (20),
+Zip int,
+PhoneNumber bigint,
+Email varchar (50)
+)
