9bb260d3c3d7e43d49340c5103088cc7f2a5b445
Payal Sharma <https://github.com/29payal>
2022-10-12 13:03:29 +0530

    [Payal] UC3-InsertValuesintoTable

--- Address_BookSQL.sql
+++ Address_BookSQL.sql
@@ -2,3 +2,26 @@
 create database Address_Book
 
 use Address_Book
+
+------UC2 create table-----
+create table ADDRESS_BOOK
+(
+Id int identity(1,1) primary key,
+FirstName varchar(20),
+LastName varchar (20),
+Address varchar(50),
+City varchar(20),
+State varchar (20),
+Zip int,
+PhoneNumber bigint,
+Email varchar (50)
+)
+
+-----UC3 insert values into the table----
+select * from ADDRESS_BOOK
+
+insert into ADDRESS_BOOK(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email)
+values('Payal', 'Sharma', '12street', 'Indore', 'Madhya Pradesh', 458441, 1234567890, 'abc@gmail.com'),
+      ('Sonu' , 'Sharma', '13 streets',' Bhopal', 'MP', 450987, 9087654321, 'red@gmail.com'),
+	  ('Aman', 'Mittal' , '14 streets', 'pune' , 'Maharashtra', 543677, 9865456789, 'aman123@gmail.com'),
+	  ('Jesna', 'Mathew', 'streets', 'bombay', 'Maharashtra', 546789, 8907623444, 'jesna12@gmail.com')
